bd89c730b72832f13ab0cb81ef1efd9ff3002374
rjatkinson2 <rjatkinson2@gmail.com>
2015-02-13 17:14:01 -0800

    Functioning enemies and draggable player properly implemented.

--- watchout.js
+++ watchout.js
@@ -1,5 +1,5 @@
 // start slingin' some d3 here.
-var Enemy = function(xAxis, yAxis, size){
+var Piece = function(xAxis, yAxis, size){
   this.xAxis = xAxis;
   this.yAxis = yAxis;
   this.size = size;
@@ -13,7 +13,7 @@ var createEnemies = function(numEnemies, size, xBoardSize, yBoardSize){
   for(var i = 0; i < numEnemies; i++){
     xAxis = Math.floor(Math.random()*(xBoardSize-size*2))+size;
     yAxis = Math.floor(Math.random()*(yBoardSize-size*2))+size;
-    enemies.push(new Enemy(xAxis,yAxis,size));
+    enemies.push(new Piece(xAxis,yAxis,size));
   }
   return enemies;
 };
@@ -21,19 +21,41 @@ var createEnemies = function(numEnemies, size, xBoardSize, yBoardSize){
 var enemies = createEnemies(50,10,700,700);
 
 // Board layout
+// Enemy layout
 d3.select("svg").selectAll("circle").data(enemies).enter()
   .append("circle").attr("cx",function(d){return d.xAxis;})
   .attr("cy",function(d){return d.yAxis;})
   .attr("r",function(d){return d.size;})
   .attr("fill","#4D997C");
 
+
+var dragmove = function() {
+  d3.select(this)
+      .attr("cx", d3.event.x)
+      .attr("cy", d3.event.y);
+};
+
+var drag = d3.behavior.drag()
+    .on("drag", dragmove);
+
+// Player layout
+var player = d3.select("svg").append("circle");
+  player.attr("cx","350").attr("cy","350").attr("r","10").attr("fill","orange");
+  player.on("click", function() {
+  if (d3.event.defaultPrevented) return; // click suppressed
+  console.log("clicked!");
+});
+  player.call(drag);
+// Enemy update
 var update = function(data) {
-  d3.selectAll("circle").data(data).transition().duration(1000).attr("cx",function(d){return d.xAxis;})
+  d3.selectAll("circle").data(data).transition().duration(2000).attr("cx",function(d){return d.xAxis;})
     .attr("cy",function(d){return d.yAxis;})
     .attr("r",function(d){return d.size;});
 };
 
+// Trigger enemy update
 setInterval(function() {
   enemies = createEnemies(50,10,700,700);
   return update(enemies);
-}, 1000);
+}, 2000);
+
