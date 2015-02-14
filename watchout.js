0ad14d877f7299f2f9bcd17b0e31d3af3ceeb296
rjatkinson2 <rjatkinson2@gmail.com>
2015-02-14 11:46:59 -0800

    Finished collision detection for enemies and players.

--- watchout.js
+++ watchout.js
@@ -22,7 +22,7 @@ var enemies = createEnemies(50,10,700,700);
 
 // Board layout
 // Enemy layout
-d3.select("svg").selectAll("circle").data(enemies).enter()
+var enemyDisplay = d3.select("svg").selectAll("circle").data(enemies).enter()
   .append("circle").attr("class","enemies").attr("cx",function(d){return d.xAxis;})
   .attr("cy",function(d){return d.yAxis;})
   .attr("r",function(d){return d.size;})
@@ -31,8 +31,6 @@ d3.select("svg").selectAll("circle").data(enemies).enter()
 
 var dragmove = function() {
   d3.select(this).attr("cx", d3.event.x).attr("cy", d3.event.y);
-  this.xAxis = d3.event.x;
-  this.yAxis = d3.event.y;
 };
 
 var drag = d3.behavior.drag()
@@ -53,22 +51,29 @@ var playerPiece = d3.select("svg").selectAll("circle.player").data(playerData).e
   playerPiece.call(drag);
 
 // Enemy update
-var update = function(data) {
-  d3.selectAll("circle.enemies").data(data).transition().duration(1000).attr("cx",function(d){return d.xAxis;})
-    .attr("cy",function(d){return d.yAxis;})
-    .attr("r",function(d){return d.size;});
+var update = function() {
+  d3.selectAll("circle.enemies").transition().duration(2000)
+    .attr("cx",function(){return Math.floor(Math.random()*(700-20))+20;})
+    .attr("cy",function(){return Math.floor(Math.random()*(700-20))+20;});
 };
+
+// Boss Mode
+// d3.selectAll("circle.enemies").transition().duration(1000)
+//   .attr("cx", Math.floor(Math.random()*(700-90))+90)
+//   .attr("cy", Math.floor(Math.random()*(700-90))+90).attr("r","90");
 
 // Trigger enemy update
 setInterval(function() {
-  enemies = createEnemies(50,10,700,700);
-  return update(enemies);
-}, 1000);
+  return update();
+}, 2000);
 
 var collision = function(player, enemy){
-  var distance = Math.sqrt(Math.pow((enemy.xAxis - player.xAxis),2) + Math.pow((enemy.yAxis - player.yAxis),2));
+  var distance = Math.sqrt(
+    Math.pow(Number(enemy.attributes.cx.value) - Number(player.attributes.cx.value),2)
+      + Math.pow(Number(enemy.attributes.cy.value) - Number(player.attributes.cy.value),2)
+        );
   var counter = 0;
-  if(distance < player.size + enemy.size){
+  if(distance < Number(player.attributes.r.value) + Number(enemy.attributes.r.value)){
     counter++;
   }
   return counter;
@@ -77,13 +82,26 @@ var collision = function(player, enemy){
 var detectCollisions = function(enemies){
   var collisions = 0;
   for(var i = 0; i < enemies.length; i++){
-    collisions += collision(player, enemies[i]);
+    collisions += collision(playerPiece[0][0], enemies[i]);
   }
   return collisions;
 };
 
+var totalCollisions = 0;
+
 setInterval(function(){
-  var enemies = d3.selectAll("circle.enemies").data();
-  console.log(detectCollisions(enemies));
+  var enemies = d3.selectAll("circle.enemies");
+  console.log(detectCollisions(enemies[0]));
 },1);
+
+setInterval(function() {
+  d3.select(".collisions").text("Collisions: " + totalCollisions);
+},100);
+
+
+
+
+
+
+
 
